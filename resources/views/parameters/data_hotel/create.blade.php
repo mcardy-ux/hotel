fbd2d8ef88ba69edc88bf13669f897756c69bfec
Juan David Cardenas <juanchodavidcardenas@outlook.com>
2021-08-04 08:38:45 -0500

    se ha configurado la maquetacion de los formularios index y create de hotel

--- resources/views/parameters/data_hotel/create.blade.php
+++ resources/views/parameters/data_hotel/create.blade.php
@@ -6,16 +6,16 @@
             <div class="row">
                 <div class="col-12">
 
-                    <h1>Form Layouts</h1>
+                    <h1>Hoteles</h1>
                     <nav class="breadcrumb-container d-none d-sm-block d-lg-inline-block" aria-label="breadcrumb">
                         <ol class="breadcrumb pt-0">
                             <li class="breadcrumb-item">
-                                <a href="#">Home</a>
+                                <a href="/dashboard">Dashboard</a>
                             </li>
                             <li class="breadcrumb-item">
-                                <a href="#">Library</a>
+                                <a href="{{route('data_hotel.index')}}">Listado</a>
                             </li>
-                            <li class="breadcrumb-item active" aria-current="page">Data</li>
+                            <li class="breadcrumb-item active" aria-current="page">Creación</li>
                         </ol>
                     </nav>
                     <div class="separator mb-5"></div>
@@ -28,28 +28,101 @@
                 <div class="col-12">
                     <div class="card mb-4">
                         <div class="card-body">
-                            <h5 class="mb-4">Basic</h5>
+                            <h5 class="mb-4">Ingresa la información necesaria.</h5>
                             <form>
                                 <div class="form-group">
-                                    <label for="exampleInputEmail1">Email address</label>
-                                    <input type="email" class="form-control" id="exampleInputEmail1"
-                                        aria-describedby="emailHelp" placeholder="Enter email">
-                                    <small id="emailHelp" class="form-text text-muted">We'll never share your email
-                                        with anyone else.</small>
+                                    <label for="razon_social">Razón Social:
+                                    </label>
+                                    <input type="text" class="form-control" id="razon_social"
+                                        aria-describedby="razonHelp" >
+                                </div>
+                               
+                                <div class="form-row">
+                                    <div class="form-group col-md-7">
+                                        <label for="razon_comercial">Razón Comercial:</label>
+                                        <input type="text" class="form-control" id="razon_comercial"
+                                            aria-describedby="razonHelp" >
+                                    </div>
+                                    <div class="form-group col-md-3">
+                                        <label for="nit">Nit:</label>
+                                        <input type="number" class="form-control" id="nit">
+                                    </div>
+                                    
+                                    <div class="form-group col-md-2">
+                                        <label for="digito_nit">Digito:</label>
+                                        <input type="number" max="10" min="1" class="form-control" id="digito_nit" >
+                                    </div>
                                 </div>
                                 <div class="form-group">
-                                    <label for="exampleInputPassword1">Password</label>
-                                    <input type="password" class="form-control" id="exampleInputPassword1"
-                                        placeholder="Password">
+                                    <label for="direccion">Dirección:
+                                    </label>
+                                    <input type="text" class="form-control" id="direccion"
+                                        aria-describedby="razonHelp" >
+                                </div>
+                                
+                                <div class="form-row">
+                                    <div class="form-group col-md-6">
+                                        <label for="telefono">Telefono:</label>
+                                        <input type="number" class="form-control" id="telefono">
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label for="telefono_alt">Telefono Alterno:</label>
+                                        <input type="number" class="form-control" id="telefono_alt" >
+                                    </div>
                                 </div>
 
-                                <div class="custom-control custom-checkbox mb-3">
-                                    <input type="checkbox" class="custom-control-input" id="customCheck1">
-                                    <label class="custom-control-label" for="customCheck1">Check this custom
-                                        checkbox</label>
+                                <div class="form-row">
+                                    <div class="form-group col-md-4">
+                                        <label for="pais">Pais:</label>
+                                        <select id="pais" class="form-control">
+                                            <option selected="">Seleccionar</option>
+                                            <option>...</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group col-md-4">
+                                        <label for="municipio">Municipio:</label>
+                                        <select id="municipio" class="form-control">
+                                            <option selected="">Seleccionar</option>
+                                            <option>...</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group col-md-4">
+                                        <label for="ciudad">Ciudad:</label>
+                                        <select id="ciudad" class="form-control">
+                                            <option selected="">Seleccionar</option>
+                                            <option>...</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="form-row">
+                                    <div class="form-group col-md-3">
+                                        <label for="resolucion_facturacion">Resolución Facturación:</label>
+                                        <select id="resolucion_facturacion" class="form-control">
+                                            <option selected="">Seleccionar</option>
+                                            <option>...</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group col-md-3">
+                                        <label for="ciiu">CIIU Actividad Economica:</label>
+                                        <select id="ciiu" class="form-control">
+                                            <option selected="">Seleccionar</option>
+                                            <option>...</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group col-md-3">
+                                        <label for="cuenta_bancaria">Cuenta Bancaria:</label>
+                                        <select id="cuenta_bancaria" class="form-control">
+                                            <option selected="">Seleccionar</option>
+                                            <option>...</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group col-md-3">
+                                        <label for="logo">Logo:</label>
+                                        <input type="file" class="form-control" id="logo" >
+                                    </div>
                                 </div>
 
-                                <button type="submit" class="btn btn-primary mb-0">Submit</button>
+                                <button type="submit" class="btn btn-primary mb-0">Agregar</button>
                             </form>
                         </div>
                     </div>
--- resources/views/parameters/data_hotel/index.blade.php
+++ resources/views/parameters/data_hotel/index.blade.php
@@ -5,11 +5,11 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-12">
-                    <h1>Listado Hoteles</h1>
+                    <h1>Hoteles</h1>
                     <nav class="breadcrumb-container d-none d-sm-block d-lg-inline-block" aria-label="breadcrumb">
                         <ol class="breadcrumb pt-0">
                             <li class="breadcrumb-item">
-                                <a href="/">Dashboard</a>
+                                <a href="/dashboard">Dashboard</a>
                             </li>
                             <li class="breadcrumb-item active" aria-current="page">Listado</li>
                         </ol>
