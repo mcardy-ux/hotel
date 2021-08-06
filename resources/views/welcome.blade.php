94edb1db5729146cb72cd4e353749221c0a4f334
Juan David Cardenas <juanchodavidcardenas@outlook.com>
2021-08-05 20:56:20 -0500

    se cambio portada del inicio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,29 +82,26 @@
 
               <div class="col-12 col-xl-4 col-lg-5 col-md-6">
                 <div class="home-text">
-                  <div class="display-1">MAGIC IS IN <br />THE DETAILS</div>
+                  <div class="display-1">Administre profesionalmente <br />su Hotel</div>
                   <p class="white mb-5">
-                    Dore is the combination of good design, quality code and attention for details.<br />
+                    Software de gestion administrativa para hoteles.<br />
 
                     <br />
-                    We used same design language for components, layouts, apps
-                    and other parts of the template. <br />
+                    Le permitira agilizar y desarrollar funciones relacionadas a rol, optimizando recursos y tiempo. <br />
                     <br />
                     Hope you enjoy it!
                   </p>
-                  <a class="btn btn-secondary btn-xl mr-2 mb-2" target="_blank" href="/Dashboard.Default.html">VIEW NOW <i
-                      class="simple-icon-arrow-right"></i></a>
-
+                  
                 </div>
               </div>
               <div class="col-12 col-xl-7 offset-xl-1 col-lg-7 col-md-6  d-none d-md-block">
-                <a target="_blank" href="/Dashboard.Default.html">
+               
                   <img alt="hero" src="img/landing-page/home-hero.png" />
-                </a>
+             
               </div>
             </div>
 
-            <div class="row">
+            <!-- <div class="row">
               <div class="col-12 p-0">
                 <div class="owl-container">
                   <div class="owl-carousel home-carousel">
@@ -113,7 +110,7 @@
                         <div>
                           <i class="iconsminds-mouse-3 large-icon"></i>
                           <h5 class="mb-3 font-weight-semibold">
-                            Right Click Menu
+                            Facil Acceso
                           </h5>
                         </div>
                         <div>
@@ -241,7 +238,7 @@
                   </div>
                 </div>
               </div>
-            </div>
+            </div> -->
 
             <div class="row">
 
@@ -253,181 +250,7 @@
 
         </div>
 
-        <div class="section">
-          <div class="container" id="features">
-            <div class="row">
-              <div class="col-12 offset-0 col-lg-8 offset-lg-2 text-center">
-                <h1>Features At a Glance</h1>
-                <p>
-                  We tried to create an admin theme that we would like to use ourselves so we listed our priorities. We
-                  would like to have a theme that is not over complicated to use, does the job well, contains must have
-                  components and looks really nice.
-                </p>
-              </div>
-            </div>
-
-            <div class="row feature-row">
-              <div class="col-12 col-md-6 col-lg-5 d-flex align-items-center">
-                <div class="d-flex">
-                  <div class="feature-icon-container">
-                    <div class="icon-background">
-                      <i class="fas fa-fw fa-ban"></i>
-                    </div>
-                  </div>
-                  <div class="feature-text-container">
-                    <h2>Pleasant Design</h2>
-                    <p>
-                      As a web developer we enjoy to work on something looks
-                      nice. It is not an absolute necessity but it really
-                      motivates us that final product will look good for user
-                      point of view. <br />
-                      <br />
-                      So we put a lot of work into colors, icons, composition
-                      and design harmony. Themed components and layouts with
-                      same design language. <br />
-                      <br />
-                      We kept user experience principles always at the heart
-                      of the design process.
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div class="col-12 col-md-6 col-lg-6 offset-lg-1 offset-md-0 position-relative">
-                <div class="background-item-1"></div>
-                <img alt="feature" class="feature-image-right feature-image-charts position-relative"
-                  src="img/landing-page/features/plesant-design.png" />
-              </div>
-            </div>
-
-            <div class="row feature-row">
-              <div class="col-12 col-md-6 col-lg-6 order-2 order-md-1">
-                <img alt="feature" class="feature-image-left feature-image-charts"
-                  src="img/landing-page/features/extra-responsive.png" />
-              </div>
-
-              <div
-                class="col-12 col-md-6 offset-md-0 col-lg-5 offset-lg-1 d-flex align-items-center order-1 order-md-2">
-                <div class="d-flex">
-                  <div class="feature-icon-container">
-                    <div class="icon-background">
-                      <i class="fas fa-fw fa-ban"></i>
-                    </div>
-                  </div>
-                  <div class="feature-text-container">
-                    <h2>Extra Responsive</h2>
-                    <p>
-                      Xxs breakpoint is for smaller screens that has a resolution lower than 420px. Xs works between
-                      576px and 420px. Xxl breakpoint is for larger screens that has a resolution higher than 1440px. Xl
-                      works between 1200px and 1440px.
-                      <br>
-                      <br>
-                      With this approach we were able to create better experiences for smaller and larger screens.
-
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div class="row feature-row">
-              <div class="col-12 col-md-6 col-lg-5 d-flex align-items-center">
-                <div class="d-flex">
-                  <div class="feature-icon-container">
-                    <div class="icon-background">
-                      <i class="fas fa-fw fa-ban"></i>
-                    </div>
-                  </div>
-                  <div class="feature-text-container">
-                    <h2>Superfine Charts</h2>
-                    <p>
-                      Using charts is a good way to visualize data but they
-                      often look ugly and break the rhythm of design. <br />
-                      <br />
-                      We concentrated on a single chart library and tried to
-                      create charts that look good with color, opacity,
-                      border and shadow. <br />
-                      <br />
-                      Used certain plugins and created some to make charts
-                      even more useful and beautiful.
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div class="col-12 col-md-6 col-lg-6 offset-lg-1 offset-md-0 ">
-                <img alt="feature" class="feature-image-right feature-image-charts"
-                  src="img/landing-page/features/superfine-charts.png" />
-              </div>
-            </div>
-
-
-            <div class="row feature-row">
-              <div class="col-12 col-md-6 col-lg-6 order-2 order-md-1">
-                <img alt="feature" class="feature-image-left feature-image-charts"
-                  src="img/landing-page/features/layouts-for-the-job.png" />
-              </div>
-
-              <div
-                class="col-12 col-md-6 offset-md-0 col-lg-5 offset-lg-1 d-flex align-items-center order-1 order-md-2">
-                <div class="d-flex">
-                  <div class="feature-icon-container">
-                    <div class="icon-background">
-                      <i class="fas fa-fw fa-ban"></i>
-                    </div>
-                  </div>
-                  <div class="feature-text-container">
-                    <h2>Layouts for the Job</h2>
-                    <p>
-                      Layouts are the real thing, they need to be accurate and
-                      right for the job. They should be functional for both
-                      user and developer. <br />
-                      <br />
-                      We created lots of different layouts for different jobs.
-                      <br />
-                      <br />
-                      Listing pages with view mode changing capabilities,
-                      shift select and select all functionality, application
-                      layouts with an additional menu, authentication and
-                      error layouts which has a different design than the
-                      other pages were our main focus. We also created details
-                      page with tabs that can hold many components.
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-
-
-            <div class="row feature-row">
-              <div class="col-12 col-md-6 col-lg-5 d-flex align-items-center">
-                <div class="d-flex">
-                  <div class="feature-icon-container">
-                    <div class="icon-background">
-                      <i class="fas fa-fw fa-ban"></i>
-                    </div>
-                  </div>
-                  <div class="feature-text-container">
-                    <h2>Smart Menu</h2>
-                    <p>
-                      Instead of good old single panel menus with accordion structure that looks over complicated, we
-                      created 2 panels and categorized pages accordingly.
-                      <br>
-                      <br>
-                      The default menu auto hides sub panel when resolution is under some breakpoint to open some space.
-                      You may also hide menu completely or use only main panel open only.
-
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div class="col-12 col-md-6 col-lg-6 offset-lg-1 offset-md-0 ">
-                <img alt="feature" class="feature-image-right feature-image-charts"
-                  src="img/landing-page/features/smart-menu.png" />
-              </div>
-            </div>
-
-          </div>
-        </div>
+    
 
 
 
