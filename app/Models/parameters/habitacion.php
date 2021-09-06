e9c6f6cc9c6c62ad664f3055be994bc247f1c2dc
Juan David Cardenas <juanchodavidcardenas@outlook.com>
2021-09-06 11:56:34 -0500

    Inicializacion base para modulo habitaciones

---
+++ app/Models/parameters/habitacion.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\parameters;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class habitacion extends Model
+{
+    use HasFactory;
+}
