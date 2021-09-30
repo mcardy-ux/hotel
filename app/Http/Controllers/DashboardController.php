4530d0bec9c285b198bda7b282bc04748af8c187
Juan David Cardenas <juanchodavidcardenas@outlook.com>
2021-09-30 14:37:01 -0500

    Se ha modificado el orden del menu lateral

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -171,11 +171,12 @@ class DashboardController extends Controller
 
    public static function mostrarMenuContable(){
 
-   $listaContable= "<li><a href='".route('planCuentas.index')."'><i class='simple-icon-directions'></i><span class='d-inline-block'>Plan de <br>Cuentas</span></a></li>";
-   $listaContable=$listaContable."<li><a href='".route('formaPago.index')."'><i class='glyph-icon iconsminds-coins-2'></i><span class='d-inline-block'>Formas de <br>Pago</span></a></li>".
+   $listaContable= "<li><a href='".route('centro.index')."'><i class='glyph-icon iconsminds-eci-icon'></i><span class='d-inline-block'>Centros</span></a></li>".
+   "<li><a href='".route('planCuentas.index')."'><i class='simple-icon-directions'></i><span class='d-inline-block'>Plan de <br>Cuentas</span></a></li>".
+   "<li><a href='".route('formaPago.index')."'><i class='glyph-icon iconsminds-coins-2'></i><span class='d-inline-block'>Formas de <br>Pago</span></a></li>".
    "<li><a href='".route('impuestos.index')."'><i class='glyph-icon iconsminds-line-chart-1'></i><span class='d-inline-block'>Impuestos</span></a></li>".
    "<li><a href='".route('agrupacionVentas.index')."'><i class='glyph-icon iconsminds-books'></i><span class='d-inline-block'>Agrupación de <br> Ventas</span></a></li>".
-   "<li><a href='".route('centro.index')."'><i class='glyph-icon iconsminds-eci-icon'></i><span class='d-inline-block'>Centros</span></a></li>".
+   
    "<li><a href='".route('codigoVenta.index')."'><i class='glyph-icon iconsminds-calendar-1'></i><span class='d-inline-block'>Codigos de <br> Venta</span></a></li>";
 
 
