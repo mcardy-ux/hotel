dc20781ccec7caa47736758470f1e9c3ce36c136
Juan David Cardenas <juanchodavidcardenas@outlook.com>
2021-10-03 12:29:20 -0500

    Se ha agregado opciones de centros como api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+//Ruta de api para obtener los documentos de base
+Route::get('/api/centro_ingreso', 'App\Http\Controllers\CentroController@getCentroIngreso')->name('api.centro_ingreso.list');
+Route::get('/api/centro_costo', 'App\Http\Controllers\CentroController@getCentroCosto')->name('api.centro_costo.list');
+Route::get('/api/centro_venta', 'App\Http\Controllers\CentroController@getCentroVenta')->name('api.centro_venta.list');
